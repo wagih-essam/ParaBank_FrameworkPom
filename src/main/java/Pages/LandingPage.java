e5e728434c8e7fad3466c26573072916a4818231
Wagih <wageh_essam2010@yahoo/com>
2024-07-25 01:41:11 +0300

    5

--- src/main/java/Pages/LandingPage.java
+++ src/main/java/Pages/LandingPage.java
@@ -2,6 +2,7 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 public class LandingPage extends BasePage {
     String url = "https://parabank.parasoft.com/parabank/index.htm";
@@ -14,6 +15,7 @@ public class LandingPage extends BasePage {
     private final By userNameField = By.xpath("//input[@name='username']");
     private final By passWordField = By.xpath("//input[@name='password']");
     private final By loginButton = By.xpath("//input[@value='Log In']");
+    private final By successfulLogin = By.xpath("//div[@id='showOverview']//h1");
     private final By forgotButton = By.xpath("//a[contains(text(), 'Forgot login info?')]\n");
     private final By registerButton = By.xpath("//a[contains(text(), 'Register')]");
     private final By aboutUsButton = By.xpath("//ul[@class='leftmenu']//a[contains(text(), 'About Us')]");
@@ -28,14 +30,22 @@ public class LandingPage extends BasePage {
         return this;
     }
 
-    public void enterUserName(String username){
+    public LandingPage enterUserName(String username){
         driver.findElement(userNameField).sendKeys(username);
+        return new LandingPage(driver);
+
     }
-    public void enterPassWord(String password){
+    public LandingPage enterPassWord(String password){
         driver.findElement(passWordField).sendKeys(password);
+        return new LandingPage(driver);
     }
-    public void setLoginButton(){
+    public LandingPage clickLoginButton(){
         driver.findElement(loginButton).click();
+        return new LandingPage(driver);
+    }
+    public void setSuccessfulLogin(){
+        String successfullogin = driver.findElement(successfulLogin).getText();
+        Assert.assertEquals(successfullogin,"Accounts Overview");
     }
     public void setForgotButton(){
         driver.findElement(forgotButton).click();
--- src/test/java/somkeTests/TestOne.java
+++ src/test/java/somkeTests/TestOne.java
@@ -6,13 +6,11 @@ import org.testng.annotations.Test;
 public class TestOne extends BaseTest {
 
     @Test
-    public void test01(){
+    public void loginFunctionality(){
         LandingPage landingPage = new LandingPage(driver);
         wait.until(d -> {
-            landingPage.navigation();
-            landingPage.enterUserName("");
-            landingPage.enterPassWord("");
-            landingPage.setLoginButton();
+            landingPage.navigation().enterUserName("usernametest")
+                    .enterPassWord("passwordtest").clickLoginButton().setSuccessfulLogin();
             return true;
         });
     }
@@ -20,20 +18,26 @@ public class TestOne extends BaseTest {
     @Test
     public void validateCurrentUrls(){
         LandingPage landingPage = new LandingPage(driver);
+        wait.until(d -> {
         landingPage.navigation().goToAboutUsButton().assertAboutUsUrl()
                 .goToLandingPage().backToPreviousPage().goToServicesButton().assertServicesUrl()
                 .goToLandingPage().backToPreviousPage().goToProductsButton().assertProductsUrl()
                 .goToLandingPage().backToPreviousPage().goToLocationsButton().assertLocationUrl()
                 .goToLandingPage().backToPreviousPage().goToAdminButton().assertAdminUrl();
+            return true;
+        });
     }
 
     @Test
     public void creatNewAccount(){
         LandingPage landingPage = new LandingPage(driver);
+        wait.until(d -> {
         landingPage.navigation().goToRegisterButton().setFirstNameField("test").setLastNameField("test")
                 .setAddressField("test address").setCityField("test city").setStateField("test state")
                 .setZipCodeField("123").setPhoneField("0123456789").setSsnField("ssn test")
-                .setUserNameRegisterField("usernametest2").setPassNameRegisterField("passwordtest")
+                .setUserNameRegisterField("usernametest3").setPassNameRegisterField("passwordtest")
                 .setConfirmRegisterButton("passwordtest").clickRegisterButton().verifySuccessfulResgister();
+        return true;
+    });
     }
 }
